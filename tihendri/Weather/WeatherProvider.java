7030b2d98a2dc6d9c0cbe982070f07851fdf9d38
tihendri <tihendri@student.wethinkcode.co.za>
2020-08-18 16:43:48 +0200

    getting weather

---
+++ tihendri/Weather/WeatherProvider.java
@@ -0,0 +1,34 @@
+package tihendri.Weather;
+
+import tihendri.Coordinates;
+import java.util.Random;
+
+/*
+** Four states of weather accounted for and passed into string array.
+** Then they are randomized in a series of ternary statements.
+*/
+
+public class WeatherProvider {
+
+    // private properties
+    private static WeatherProvider weatherProvider = new WeatherProvider();
+    private String[] weather = {"RAIN", "FOG", "SUN", "SNOW"};
+
+    // private constructor
+    private WeatherProvider() {
+    }
+
+    // public methods
+    public static WeatherProvider getProvider() {
+        return weatherProvider;
+    }
+
+    public String getCurrentWeather(Coordinates coordinates) {
+        int randWeather = new Random().nextInt(weather.length);
+        randWeather = (coordinates.getHeight() > 20) ? randWeather++ : randWeather--;
+        randWeather = (randWeather < 0) ? 3 : randWeather;
+        randWeather = (randWeather > 3) ? 0 : randWeather;
+        return weather[randWeather];
+    }
+
+}
