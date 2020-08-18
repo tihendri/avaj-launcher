49fab7a9020ac2616f3b389938519ea149c0ef60
tihendri <tihendri@student.wethinkcode.co.za>
2020-08-18 16:02:28 +0200

    check UML diagram for cross-referencing methods

---
+++ tihendri/Coordinates.java
@@ -0,0 +1,48 @@
+package tihendri;
+
+/*
+** All coordinates that will be used in the simulation. Coordinates will be the same for each aircraft, but
+** will change according to the four weather conditions.
+*/
+
+public class Coordinates {
+    
+    // private properties
+    private int height;
+    private int latitude;
+    private int longitude;
+
+    // default constructor
+    Coordinates(int longitude, int latitude, int height) {
+        if (height > 100)
+            this.height = 100;
+        else if (height < 0)
+            this.height = 0;
+        else
+            this.height = height;
+        
+        if (latitude < 0)
+            latitude = 0;
+        
+        if (longitude < 0)
+            longitude = 0;
+
+        this.longitude = longitude;
+        this.latitude = latitude;
+        return ;
+    }
+
+    // getters
+    public int getHeight() {
+        return this.height;
+    }
+
+    public int getLatitude() {
+        return this.latitude;
+    }
+
+    public int getLongitude() {
+        return this.longitude;
+    }
+
+}
