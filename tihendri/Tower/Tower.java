5870e4f66714f7e36096d7ee82d399bd6b329226
tihendri <tihendri@student.wethinkcode.co.za>
2020-08-24 23:23:06 +0200

    added new file, not done

---
+++ tihendri/Tower/Tower.java
@@ -0,0 +1,18 @@
+package tihendri.Tower;
+
+import tihendri.Interface.Flyable;
+
+import java.util.ArrayList;
+
+public class Tower
+{
+    private ArrayList<Flyable> unsubscribe = new ArrayList<>();
+    private ArrayList<Flyable> observers = new ArrayList<>();
+
+    public void register(Flyable flyable)
+
+
+
+
+
+}
