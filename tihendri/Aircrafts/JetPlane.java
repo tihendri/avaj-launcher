a76f5ea7d0e4108e78d6f6f5c0833550c8c0282e
tihendri <tihendri@student.wethinkcode.co.za>
2020-08-25 21:04:49 +0200

    Added all aircraft types

--- tihendri/Aircrafts/JetPlane.java
+++ tihendri/Aircrafts/JetPlane.java
@@ -1,4 +1,34 @@
 package tihendri.Aircrafts;
 
-public class JetPlane {
+import tihendri.Coordinates.Coordinates;
+import tihendri.Interface.Flyable;
+import tihendri.Weather.WeatherTower;
+
+public class JetPlane extends Aircraft implements Flyable {
+    private WeatherTower weatherTower = new WeatherTower();
+
+    protected JetPlane(String type, Coordinates coordinates) {
+        super(type, coordinates);
+    }
+
+    @Override
+    public void updateConditions() {
+        String weather = weatherTower.getWeather(coordinates);
+        updateCoordinates(this, weather, weatherTower);
+    }
+
+    @Override
+    public void registerTower(WeatherTower weatherTower) {
+        weatherTower.register(this);
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getType() {
+        return super.getType();
+    }
 }
