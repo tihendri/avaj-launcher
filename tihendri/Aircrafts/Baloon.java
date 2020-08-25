b12ecf709f6088d31d3af5562ab0269762e2f6ec
tihendri <tihendri@student.wethinkcode.co.za>
2020-08-25 21:05:11 +0200

    Added all aircraft types

--- tihendri/Aircrafts/Baloon.java
+++ tihendri/Aircrafts/Baloon.java
@@ -1,4 +1,40 @@
 package tihendri.Aircrafts;
 
-public class Baloon {
+import tihendri.Coordinates.Coordinates;
+import tihendri.Interface.Flyable;
+import tihendri.Weather.WeatherTower;
+
+public class Baloon extends Aircraft implements Flyable
+{
+
+    protected Baloon(String type, Coordinates coordinates) {
+        super(type, coordinates);
+    }
+
+    private WeatherTower weatherTower = new WeatherTower();
+
+    @Override
+    public void updateConditions() {
+        String weather = weatherTower.getWeather(coordinates);
+        updateCoordinates(this,weather,weatherTower);
+    }
+
+    public void updateCoordinates(Flyable flyable, String weather, WeatherTower weatherTower) {
+
+    }
+
+    @Override
+    public void registerTower(WeatherTower weatherTower) {
+        weatherTower.register(this);
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getType() {
+        return super.getType();
+    }
 }
