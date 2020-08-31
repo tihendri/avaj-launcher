e2b4cf7b908909bcb09862c1ae04cd868208655b
tihendri <tihendri@student.wethinkcode.co.za>
2020-08-31 16:32:34 +0200

    Enhanced switch statements apparently don't compile with java 8

--- tihendri/Aircrafts/Aircraft.java
+++ tihendri/Aircrafts/Aircraft.java
@@ -1,6 +1,7 @@
 package tihendri.Aircrafts;
 
 import tihendri.Coordinates.Coordinates;
+import tihendri.ErrorHandling.Exceptions.CustomException;
 import tihendri.Interface.Flyable;
 import tihendri.Weather.WeatherTower;
 
@@ -10,27 +11,29 @@ public class Aircraft
     protected long aircraftId;
     protected Coordinates coordinates;
 
-    private long counter = 0;
+    private static long counter = 0;
 
     private long nextId()
     {
-        this.counter++;
+        counter += 1;
         return (counter);
-    }
-
-    protected Aircraft(String type, Coordinates coordinates)
-    {
-        this.type = type;
-        this.coordinates = coordinates;
-        this.aircraftId = nextId();
     }
 
     public Coordinates getCoordinates() {
         return this.coordinates;
     }
 
+    protected Aircraft(String type, Coordinates coordinates)
+    {
+        this.type = type;
+        this.coordinates = coordinates;
+        if (coordinates.getHeight() > 0) {
+            this.aircraftId = nextId();
+        }
+    }
+
     public String getType() {
-        return this.type;
+        return type;
     }
 
     public String getId() {
@@ -44,74 +47,89 @@ public class Aircraft
         }
     }
 
-    private void sunWeather(String type) {
-
-        switch (type) {
-            case "Baloon" -> {
+    private void sunWeather(String aircraftType) {
+        switch (aircraftType) {
+            case "Baloon":
                 coordinates.setLongitude(coordinates.getLongitude() + 2);
                 coordinates.setHeight(coordinates.getHeight() + 4);
-            }
-            case "Helicopter" -> {
+                break;
+            case "Helicopter":
                 coordinates.setLongitude(coordinates.getLongitude() + 10);
                 coordinates.setHeight(coordinates.getHeight() + 2);
-            }
-            case "JetPlane" -> {
+                break;
+            case "JetPlane":
                 coordinates.setLatitude(coordinates.getLatitude() + 10);
                 coordinates.setHeight(coordinates.getHeight() + 2);
-            }
+                break;
         }
     }
 
-    private void rainWeather(String type) {
-        switch (type) {
-            case "Baloon" -> coordinates.setHeight(coordinates.getHeight() - 5);
-            case "Helicopter" -> coordinates.setLongitude(coordinates.getLongitude() + 5);
-            case "JetPlane" -> coordinates.setLatitude(coordinates.getLatitude() + 5);
+    private void rainWeather(String aircraftType) {
+        switch (aircraftType) {
+            case "Baloon":
+                coordinates.setHeight(coordinates.getHeight() - 5);
+                break;
+            case "Helicopter":
+                coordinates.setLongitude(coordinates.getLongitude() + 5);
+                break;
+            case "JetPlane":
+                coordinates.setLatitude(coordinates.getLatitude() + 5);
+                break;
         }
     }
 
-    private void fogWeather(String type) {
-        switch (type) {
-            case "Baloon" -> coordinates.setHeight(coordinates.getHeight() - 3);
-            case "Helicopter" -> coordinates.setLongitude(coordinates.getLongitude() + 1);
-            case "JetPlane" -> coordinates.setLatitude(coordinates.getLatitude() + 1);
+    private void fogWeather(String aircraftType) {
+        switch (aircraftType) {
+            case "Baloon":
+                coordinates.setHeight(coordinates.getHeight() - 3);
+                break;
+            case "Helicopter":
+                coordinates.setLongitude(coordinates.getLongitude() + 1);
+                break;
+            case "JetPlane":
+                coordinates.setLatitude(coordinates.getLatitude() + 1);
+                break;
         }
     }
 
-    private void snowWeather(String type) {
-        switch (type) {
-            case "Baloon" -> coordinates.setHeight(coordinates.getHeight() - 15);
-            case "Helicopter" -> coordinates.setHeight(coordinates.getHeight() - 12);
-            case "JetPlane" -> coordinates.setHeight(coordinates.getHeight() - 7);
+    private void snowWeather(String aircraftType) {
+        switch (aircraftType) {
+            case "Baloon":
+                coordinates.setHeight(coordinates.getHeight() - 15);
+                break;
+            case "Helicopter":
+                coordinates.setHeight(coordinates.getHeight() - 12);
+                break;
+            case "JetPlane":
+                coordinates.setHeight(coordinates.getHeight() - 7);
+                break;
         }
     }
 
     public void updateCoordinates(Flyable flyable, String weather, WeatherTower weatherTower) {
         String aircraftType = flyable.getType();
         String aircraftName = flyable.getName();
-        String aircraftId = flyable.getId();
+        String airId = flyable.getId();
 
         switch (weather) {
-            case "RAIN" -> {
-                rainWeather(aircraftType);
-                System.out.println(aircraftType + "#" + aircraftName + '(' + aircraftId + "): Hallelujah it's a raining man!");
-            }
-            case "SNOW" -> {
-                snowWeather(aircraftType);
-                System.out.println(aircraftType + "#" + aircraftName + '(' + aircraftId + "): SJW: This snow has some serious privilege... xD ");
-            }
-            case "SUN" -> {
-                sunWeather(aircraftType);
-                System.out.println(aircraftType + "#" + aircraftName + '(' + aircraftId + "): Sun's out, guns out. But I don't have any... :[ ");
-            }
-            case "FOG" -> {
-                fogWeather(aircraftType);
-                System.out.println(aircraftType + "#" + aircraftName + '(' + aircraftId + "): Fog off!");
-            }
-            default -> {
-                System.out.println((char) 27 + "[33mError: Big oof!" + (char) 27 + "[0m");
-                System.exit(1);
-            }
+            case "RAIN":
+                rainWeather(aircraftName);
+                System.out.println(aircraftName + "#" + aircraftType + '(' + airId + "): Hallelujah it's a raining man!");
+                break;
+            case "SNOW":
+                snowWeather(aircraftName);
+                System.out.println(aircraftName + "#" + aircraftType + '(' + airId + "): SJW: This snow has white privilege... xD ");
+                break;
+            case "SUN":
+                sunWeather(aircraftName);
+                System.out.println(aircraftName + "#" + aircraftType + '(' + airId + "): Sun's out, guns out. But don't shoot me please... :[ ");
+                break;
+            case "FOG":
+                fogWeather(aircraftName);
+                System.out.println(aircraftName + "#" + aircraftType + '(' + airId + "): For fog sake! I can't see anything");
+                break;
+            default:
+                throw new CustomException((char) 27 + "[33mError: Big oof!" + (char) 27 + "[0m");
         }
         if (coordinates.getHeight() <= 0) {
             weatherTower.unregister(flyable);
