4d1bf9a450f4eb31db048a2fd1b799145e2a4c57
tihendri <tihendri@student.wethinkcode.co.za>
2020-08-31 15:39:05 +0200

    Swapped method names to display proper output

--- tihendri/Tower/Tower.java
+++ tihendri/Tower/Tower.java
@@ -5,29 +5,28 @@ import tihendri.Interface.Flyable;
 import java.util.ArrayList;
 
 public class Tower {
-//    private ArrayList<Flyable> unsubscribe = new ArrayList<>();
-    private ArrayList<Flyable> observers = new ArrayList<>();
+    private final ArrayList<Flyable> observers = new ArrayList<>();
 
     public void register(Flyable flyable) {
         if (flyable.getCoordinates().getHeight() > 0) {
             observers.add(flyable);
-            System.out.println("Tower says: " + flyable.getType() + "#" + flyable.getName() + '(' + flyable.getId() + ") registered to the weather tower.");
+            System.out.println("Tower says: " + flyable.getName() + "#" + flyable.getType() + '(' + flyable.getId() + ") registered to the weather tower.");
         }
     }
 
     public void unregister(Flyable flyable) {
-        System.out.println(flyable.getType() + "#" + flyable.getName() + "(" + flyable.getId() + ")" + " landing.");
-        System.out.println("Tower says: " + flyable.getType() + "#" + flyable.getName() + "(" + flyable.getId() + ") unregistered from weather tower.");
-        System.out.println(flyable.getType() + "#" + flyable.getName() + "(" + flyable.getId() + ") Coordinates(Longitude = " + flyable.getCoordinates().getLongitude() + " and Latitude = " + flyable.getCoordinates().getLatitude() + ")");
+        System.out.println(flyable.getName() + "#" + flyable.getType() + "(" + flyable.getId() + ")" + " landing.");
+        System.out.println("Tower says: " + flyable.getName() + "#" + flyable.getType() + "(" + flyable.getId() + ") unregistered from weather tower.");
+        System.out.println(flyable.getName() + "#" + flyable.getType() + "(" + flyable.getId() + ") Coordinates(Longitude = " + flyable.getCoordinates().getLongitude() + " and Latitude = " + flyable.getCoordinates().getLatitude() + ")");
     }
 
     protected void conditionsChanged() {
-        for (int count = 0; count < observers.size(); count++) {
-            observers.get(count).updateConditions();
+        for (int counter = 0; counter < observers.size(); counter++) {
+            observers.get(counter).updateConditions();
 
-            if (observers.get(count).getCoordinates().getHeight() <= 0) {
-                observers.remove(count);
-                count--;
+            if (observers.get(counter).getCoordinates().getHeight() <= 0) {
+                observers.remove(counter);
+                counter--;
             }
         }
     }
